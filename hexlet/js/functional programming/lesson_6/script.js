c2856e7f23a41df7516b96cf2c90239693203cfa
Anastasia2504 <60847311+Anastasia2504@users.noreply.github.com>
2020-05-13 19:55:30 +0300

    Function

---
+++ hexlet/js/functional programming/lesson_6/script.js
@@ -0,0 +1,14 @@
+
+
+function reverse(str, ind = str.length - 1, acc = '') {
+    // ind = str.length - 1;
+    if(ind === -1) {
+      return acc;
+    } else{
+      console.log(str[ind]);
+      return reverse(str, ind - 1, acc + str[ind]);
+    }
+
+    
+}
+console.log(reverse("strvfvs"));
