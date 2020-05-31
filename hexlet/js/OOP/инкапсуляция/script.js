93b2db8b88cc8d4e39171586cf6d8cdd6e01741e
Anastasia2504 <60847311+Anastasia2504@users.noreply.github.com>
2020-05-31 12:31:44 +0300

    инкапсуляция2

--- hexlet/js/OOP/инкапсуляция/script.js
+++ hexlet/js/OOP/инкапсуляция/script.js
@@ -1,11 +1,45 @@
-const user = ({ id = null, friends = [] } = {}) => ({
+const makeUser = ({
+  id = null,
+  friends = []
+} = {}) => ({
   friends,
   id,
   getFriends() {
     return this.friends;
   },
 });
-console.log(users)
+const users = [
+  makeUser({
+    id: 2
+  }),
+  makeUser({
+    id: 8
+  }),
+  makeUser({
+    id: 7
+  }),
+  makeUser({
+    id: 100
+  }),
+];
+const user1 = makeUser({
+  friends: [users[0], users[1], users[3]]
+});
+const user2 = makeUser({
+  friends: [users[0], makeUser({
+    id: 100
+  }), users[2]]
+});
+console.log(user1);
+// деструктуктизация
+// user({id: "qwe", friends: ["213", "23"]})
 
-user({id: "qwe", friends: ["213", "23"]})
-
+const getMutualFriends = (user1, user2) => {
+    const friends1 = user1.getFriends();
+    const friends2 = user2.getFriends();
+    // the esiest way to do it is using intersection function from lodash
+    const mutualFriends = friends1.filter((friend1) =>
+      friends2.some((friend2) => friend2.id === friend1.id));
+    return mutualFriends;
+  };
+console.log(getMutualFriends(user1, user2));
