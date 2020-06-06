adb158ffe067ae9de2ca2ca33768f5723efa75fe
Anastasia2504 <60847311+Anastasia2504@users.noreply.github.com>
2020-06-06 22:20:02 +0300

    каррирование

---
+++ hexlet/js/function/каррирование/script.js
@@ -0,0 +1,5 @@
+const isEven = (number) => (number % 2 === 0);
+const check = (number) => {
+  const result = isEven(number) ? 'yes' : 'no';
+  console.log(result);
+};
---
+++ hexlet/js/functional programming/каррирование/script.js
@@ -0,0 +1,81 @@
+const isEven = (number) => (number % 2 === 0);
+const check = (number) => {
+  const result = isEven(number) ? 'yes' : 'no';
+  console.log(result);
+};
+// const sum2 = a => b => c => a + b + c
+// вызовы происходят без создания промежуточных констант
+const sum2 = (a) => {
+  return (b) => {
+    return (c) => {
+      return a + b + c;
+    };
+  };
+};
+const sum = (a, b, c) => a + b + c;
+console.log(sum(5, 10, -2));
+console.log(sum2(5)(10)(-2));
+
+function True() {
+  return (a) => {
+    return () => {
+      return a;
+    }
+  }
+}
+console.log(True()(2)());
+function False() {
+  return () => {
+    return (b) => {
+      return b;
+    }
+  }
+}
+console.log(False()()(2));
+function If(True) {
+  return True();
+};
+console.log(If(True)(2)());
+// Пример использования функции:
+If(True)('one')('two'); // 'one'
+
+// Передаём в функцию If функцию True, возвращается так же функция.
+If(True); // (x) => () => x
+
+// В эту функцию передаём в качестве аргумента x значение 'one'
+// и делаем вызов. Получаем вложенную функцию.
+If(True)('one'); // () => 'one'
+
+// Вызываем функцию с предыдущего шага и передаём в неё 'two'.
+// Это значение игнорируется, так как вложенная функция
+// не принимает аргументов и возвращает значение 'one'.
+If(True)('one')('two'); // 'one'
+
+// С False функция If работает аналогично.
+If(False)('one')('two'); // 'two'
+
+// Альтернативный способ определения функций:
+const True = (x) => {
+  return () => {
+    return x;
+  };
+};
+
+const False = () => {
+  return (y) => {
+    return y;
+  };
+};
+
+const If = (f) => {
+  return f;
+};
+*/
+const IfCheck = (a) => {
+  return (b) => {
+    return (c) => {
+      return (a == 'True') ? b : c;
+    };
+  };
+};
+IfCheck('True')('one')('two');
